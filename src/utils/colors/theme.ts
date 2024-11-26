3646ae4b2dd785b940d98ee951bd025c8bbe7148
Bruno Vieira de Moura <bruno.moura.code@gmail.com>
2024-11-26 10:58:37 -0300

    Definida as pastas, adicionado theme e typografia

---
+++ src/utils/colors/theme.ts
@@ -0,0 +1,169 @@
+'use client';
+import { type_first, type_second } from '@/utils/fonts/font';
+import { createTheme } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface BreakpointOverrides {
+    xs: false;
+    sm: false;
+    md: false;
+    lg: false;
+    xl: false;
+    mobile: true;
+    tablet: true;
+    laptop: true;
+    desktop: true;
+  }
+}
+
+const theme = createTheme({
+  spacing: 8,
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 650,
+      laptop: 900,
+      desktop: 1200,
+    },
+  },
+  palette: {
+    mode: 'light',
+    common: {
+      black: '#000000',
+      white: '#ffffff',
+    },
+    primary: {
+      main: '#1976d2',
+      light: '#42a5f5',
+      dark: '#1565c0',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#9c27b0',
+      light: '#ba68c8',
+      dark: '#7b1fa2',
+      contrastText: '#ffffff',
+    },
+    error: {
+      main: '#d32f2f',
+      light: '#ef5350',
+      dark: '#c62828',
+      contrastText: '#ffffff',
+    },
+    warning: {
+      main: '#ed6c02',
+      light: '#ff9800',
+      dark: '#e65100',
+      contrastText: '#ffffff',
+    },
+    info: {
+      main: '#D6F6F9',
+      light: '#03a9f4',
+      dark: '#01579b',
+      contrastText: '#ffffff',
+    },
+    success: {
+      main: '#2e7d32',
+      light: '#4caf50',
+      dark: '#1b5e20',
+      contrastText: '#ffffff',
+    },
+    grey: {
+      50: '#fafafa',
+      100: '#f5f5f5',
+      200: '#eeeeee',
+      300: '#e0e0e0',
+      400: '#bdbdbd',
+      500: '#9e9e9e',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
+      A100: '#f5f5f5',
+      A200: '#eeeeee',
+      A400: '#bdbdbd',
+      A700: '#616161',
+    },
+  },
+  typography: {
+    htmlFontSize: 16,
+    fontFamily: type_first.style.fontFamily,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+    h1: {
+      fontFamily: type_first.style.fontFamily,
+      fontWeight: 300,
+      fontSize: '6rem',
+      lineHeight: 1.167,
+      letterSpacing: '-0.01562em',
+    },
+    h2: {
+      fontFamily: type_first.style.fontFamily,
+      fontWeight: 300,
+      fontSize: '3.75rem',
+      lineHeight: 1.2,
+      letterSpacing: '-0.00833em',
+    },
+    h3: {
+      fontFamily: type_first.style.fontFamily,
+      fontWeight: 400,
+      fontSize: '3rem',
+      lineHeight: 1.167,
+      letterSpacing: '0em',
+    },
+    h4: {
+      fontFamily: type_first.style.fontFamily,
+      fontWeight: 400,
+      fontSize: '2.125rem',
+      lineHeight: 1.235,
+      letterSpacing: '0.00735em',
+    },
+    h5: {
+      fontFamily: type_second.style.fontFamily,
+      fontWeight: 400,
+      fontSize: '1.5rem',
+      lineHeight: 1.334,
+      letterSpacing: '0em',
+    },
+    h6: {
+      fontFamily: type_second.style.fontFamily,
+      fontWeight: 500,
+      fontSize: '1.25rem',
+      lineHeight: 1.6,
+      letterSpacing: '0.0075em',
+    },
+    subtitle1: {
+      fontFamily: type_second.style.fontFamily,
+      fontWeight: 400,
+      fontSize: '1rem',
+      lineHeight: 1.75,
+      letterSpacing: '0.00938em',
+    },
+    subtitle2: {
+      fontFamily: type_second.style.fontFamily,
+      fontWeight: 500,
+      fontSize: '0.875rem',
+      lineHeight: 1.57,
+      letterSpacing: '0.00714em',
+    },
+    body1: {
+      fontFamily: type_second.style.fontFamily,
+      fontWeight: 400,
+      fontSize: '1rem',
+      lineHeight: 1.5,
+      letterSpacing: '0.00938em',
+    },
+    body2: {
+      fontFamily: type_second.style.fontFamily,
+      fontWeight: 400,
+      fontSize: '0.875rem',
+      lineHeight: 1.43,
+      letterSpacing: '0.01071em',
+    },
+  },
+});
+
+export default theme;
