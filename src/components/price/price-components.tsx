1c1ddefabc870b0d7646cd392949fa4eaffb6b3e
Bruno Vieira de Moura <bruno.moura.code@gmail.com>
2024-11-28 20:15:45 -0300

    plan price desktop

--- src/components/price/price-components.tsx
+++ src/components/price/price-components.tsx
@@ -3,11 +3,34 @@
 import { Box, Paper, Typography, Button } from '@mui/material';
 import GenericList from './list-generic';
 import { GenericListPrice } from './list-obj';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 export default function PriceComponent() {
   return (
-    <div style={{ height: '100%' }}>
-      <Box sx={{ mt: 4, p: 2 }}>
+    <Box
+      sx={{
+        height: {
+          xs: '100vh',
+          sm: '100vh',
+          md: '100vh',
+        },
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          mt: 4,
+          p: 2,
+          maxWidth: '70rem',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
         <Typography
           gutterBottom
           textAlign="center"
@@ -17,16 +40,285 @@ export default function PriceComponent() {
           Escolha o plano perfeito para{' '}
           <span style={{ color: '#a2c41e' }}>você</span>.
         </Typography>
-        <Typography textAlign="center" variant="body1" color="initial">
-          Experimente o fit<span style={{ color: '#a2c41e' }}>boost</span>. sem
-          anúncios e com todas suas ferramentas para acesso.
+        <Typography
+          sx={{
+            width: {
+              xs: '100%',
+              sm: '100%',
+              md: '60%',
+            },
+            textAlign: 'center',
+          }}
+          textAlign="center"
+          variant="body1"
+          color="initial"
+        >
+          Teste <span style={{ color: '#a2c41e' }}>Grátis por 7 dias, </span>sem
+          preocupações. Você pode alterar ou cancelar seu plano quando quiser.
         </Typography>
       </Box>
-      <div
-        style={{
+
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr',
+          gap: 2,
+          width: '50%',
+          height: '380px',
+        }}
+      >
+        {/* PLANO PADRÃO DESK */}
+        <Box
+          sx={{
+            display: {
+              xs: 'none',
+              sm: 'none',
+              md: 'flex',
+            },
+            justifyContent: 'space-between',
+            flexDirection: 'column',
+            gap: 2,
+            border: '2px solid #1C1B1B',
+            padding: '16px',
+            borderRadius: '8px',
+          }}
+        >
+          <Typography textAlign="center" variant="h4" sx={{ color: '#1C1B1B' }}>
+            Plano Padrão
+          </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: 2,
+              }}
+            >
+              <CheckCircleIcon />
+              <Typography
+                sx={{ color: '#1C1B1B' }}
+                variant="body1"
+                color="initial"
+              >
+                Treinos com video aulas semanais
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: 2,
+              }}
+            >
+              <CheckCircleIcon />
+              <Typography
+                sx={{ color: '#1C1B1B' }}
+                variant="body1"
+                color="initial"
+              >
+                Treinos personalizados
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: 2,
+              }}
+            >
+              <CheckCircleIcon />
+              <Typography
+                sx={{ color: '#1C1B1B' }}
+                variant="body1"
+                color="initial"
+              >
+                Alertas customizados
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: 2,
+              }}
+            >
+              <CheckCircleIcon />
+              <Typography
+                sx={{ color: '#1C1B1B' }}
+                variant="body1"
+                color="initial"
+              >
+                Sem anúncios
+              </Typography>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'end',
+            }}
+          >
+            <Typography variant="body1" sx={{ color: '#1C1B1B' }}>
+              R$
+            </Typography>
+            <Typography variant="h4" sx={{ color: '#1C1B1B' }}>
+              12,90
+            </Typography>
+            <Typography variant="body1" sx={{ color: '#1C1B1B' }}>
+              /mês
+            </Typography>
+          </Box>
+          <Button
+            sx={{
+              color: '#1C1B1B',
+              border: '2px solid #1c1b1b',
+              fontWeight: 'bold',
+              '&:hover': {
+                color: '#f3f3f3',
+                backgroundColor: '#1C1B1B',
+                outline: '',
+              },
+            }}
+            variant="outlined"
+          >
+            Assinar Plano padrão
+          </Button>
+        </Box>
+        {/* PLANO BOOST DESK */}
+        <Box
+          sx={{
+            position: 'relative',
+            display: {
+              xs: 'none',
+              sm: 'none',
+              md: 'flex',
+            },
+            justifyContent: 'space-between',
+            flexDirection: 'column',
+            gap: 2,
+            border: '2px solid #1C1B1B',
+            padding: '16px',
+            borderRadius: '8px',
+            background: '#1C1B1B',
+          }}
+        >
+          <div
+            style={{
+              color: '#333',
+              display: 'flex',
+              background: '#BBEE33',
+              position: 'absolute',
+              width: '140px',
+              top: '-14px',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              left: '0',
+              right: '0',
+              justifyContent: 'center',
+              borderRadius: '2px',
+              padding: '2px',
+              fontWeight: 'bold',
+            }}
+          >
+            <p>Mais popular</p>
+          </div>
+          <Typography textAlign="center" variant="h4" color="secondary.light">
+            Plano <span style={{ color: '#bbee33' }}>boost</span>
+          </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: 2,
+              }}
+            >
+              <CheckCircleIcon sx={{ color: '#bbee33' }} />
+              <Typography variant="body1" color="secondary.light">
+                Gráficos de gerenciamento exclusivo alimentar e físico.
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: 2,
+              }}
+            >
+              <CheckCircleIcon sx={{ color: '#bbee33' }} />
+              <Typography color="secondary.light" variant="body1">
+                Personal treiner online
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: 2,
+              }}
+            >
+              <CheckCircleIcon sx={{ color: '#f3f3f3' }} />
+              <Typography color="secondary.light" variant="body1">
+                Treinos com video aulas semanais
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: 2,
+              }}
+            >
+              <CheckCircleIcon sx={{ color: '#f3f3f3' }} />
+              <Typography variant="body1" color="secondary.light">
+                Treinos personalizados
+              </Typography>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'end',
+            }}
+          >
+            <Typography variant="body1" sx={{ color: '#f3f3f3' }}>
+              R$
+            </Typography>
+            <Typography variant="h4" sx={{ color: '#f3f3f3' }}>
+              16,90
+            </Typography>
+            <Typography variant="body1" sx={{ color: '#f3f3f3' }}>
+              /mês
+            </Typography>
+          </Box>
+          <Button
+            sx={{
+              color: '#1C1B1B',
+              fontWeight: 'bold',
+              '&:hover': {
+                color: '#a2c41e',
+                backgroundColor: 'transparent',
+                outline: '2px solid #a2c41e',
+              },
+            }}
+            variant="contained"
+          >
+            Assinar Plano boost
+          </Button>
+        </Box>
+      </Box>
+      {/* MOBILE */}
+      <Box
+        sx={{
           width: '100%',
           background: '#FBFBFB',
-          display: 'flex',
+          display: {
+            xs: 'flex',
+            sm: 'flex',
+            md: 'none',
+          },
           flexWrap: 'wrap',
           flexDirection: 'column',
           gap: '16px 0px',
@@ -154,8 +446,17 @@ export default function PriceComponent() {
             />
           </div>
         ))}
-      </div>
-      <Box sx={{ p: 2 }}>
+      </Box>
+      <Box
+        sx={{
+          p: 2,
+          display: {
+            xs: 'flex',
+            sm: 'flex',
+            md: 'none',
+          },
+        }}
+      >
         <Button
           sx={{ color: '#1C1B1B', fontWeight: '600' }}
           fullWidth
@@ -166,6 +467,6 @@ export default function PriceComponent() {
           Experimente grátis por 7 dias!
         </Button>
       </Box>
-    </div>
+    </Box>
   );
 }
